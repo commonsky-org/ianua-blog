cd4cc97731e8e83708fe9f348f88f24a04e0c333
tyayers <tyler.ayers@gmail.com>
2025-03-09 15:05:39 +0100

    updated

--- 1.deploy.cloudflare.sh
+++ 1.deploy.cloudflare.sh
@@ -1,4 +1,4 @@
 nvm use 21
 npm run build
-npx wrangler pages deploy ./dist --project-name plnet-apps
+npx wrangler pages deploy ./dist --project-name planet-apps
 git stage . && git commit -m "updated" && git push origin main
